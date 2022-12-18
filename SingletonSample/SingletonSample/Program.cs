4064300910ff9f274161518d37243375061111f9
sinare0019 <sina.rezaei.pr1@gmail.com>
2022-12-18 20:55:48 +0330

    add singleton

---
+++ SingletonSample/SingletonSample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Score score1 = Score.GetInstance();
+            Score score2 = Score.GetInstance();
+            score1.Number = 5;
+            //در این قسمت مشاهده میکنیم که دیگر 2 ابجکت متفاوت تعریف نشد بلکه هر دو با هم برابرند
+            Console.WriteLine(score2.Number);           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SingletonSample/SingletonSample/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonSample
+{
+    public class Score
+    {
+
+        private static Score instance;
+        private Score() { }
+        public int Number { set; get; }
+
+        public static Score GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Score();
+            }
+            return instance;
+        }
+    }
+}
